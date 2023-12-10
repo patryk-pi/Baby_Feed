286f865459dadd726593e756ac44e8364658cf40
patryk-pi <patryk.piasecki1@gmail.com>
2023-12-10 22:58:48 +0100

    added Hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,8 @@
+const Hero = () => {
+    return (
+        <section className="hero">
+            <video src="https://youtu.be/bYrCUJxjl7M"></video>
+        </section>
+    );
+};
+export default Hero;
