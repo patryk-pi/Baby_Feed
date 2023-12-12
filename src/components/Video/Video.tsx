31e3f301a095b40187e2ec5eb1ed11eb5cdb3532
patryk-pi <patryk.piasecki1@gmail.com>
2023-12-12 15:14:38 +0100

    added playsInline attribute in video

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,7 +1,7 @@
 const Video = () => {
     return (
         <section className="video">
-            <video autoPlay loop muted>
+            <video autoPlay loop muted playsInline>
                 <source src="/DJVid.mp4" type="video/mp4" />
             </video>
         </section>
