fe62126d5f23b692aaca3a81985aae5f80ae843c
patryk-pi <patryk.piasecki1@gmail.com>
2023-12-14 13:32:51 +0100

    changed styling in hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -14,14 +14,16 @@ const Hero = () => {
     return (
         <section className="hero">
             <h1 className="hero__header">DJ na każdy event</h1>
-            <Event
-                title={weddingTitle}
-                text1={weddingText1}
-                text2={weddingText2}
-                text3={weddingText3}
-            />
-            <Event title={companyTitle} text1={companyText} />
-            <Event title={partyTitle} text1={partyText} />
+            <div className="hero__container">
+                <Event
+                    title={weddingTitle}
+                    text1={weddingText1}
+                    text2={weddingText2}
+                    text3={weddingText3}
+                />
+                <Event title={companyTitle} text1={companyText} />
+                <Event title={partyTitle} text1={partyText} />
+            </div>
         </section>
     );
 };
