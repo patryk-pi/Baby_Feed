3ef7a61bfe2bba9df7d4377d48d17d5d55eb7486
patryk-pi <patryk.piasecki1@gmail.com>
2023-12-15 21:11:08 +0100

    added animation on event component

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,10 +1,16 @@
+"use client";
+
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
+
 const Video = () => {
     return (
-        <section className="video">
-            <video autoPlay loop muted playsInline>
-                <source src="/DJVid.mp4" type="video/mp4" />
-            </video>
-        </section>
+        <ParallaxProvider>
+            <section className="video">
+                <video autoPlay loop muted playsInline>
+                    <source src="/DJVid.mp4" type="video/mp4" />
+                </video>
+            </section>
+        </ParallaxProvider>
     );
 };
 export default Video;
