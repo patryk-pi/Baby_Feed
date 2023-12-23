786449abd082643f798dc91f4b1ab19be358721d
patryk-pi <patryk.piasecki1@gmail.com>
2023-12-23 15:30:40 +0100

    styled contact section

--- src/components/ContactPage/Contact/Contact.tsx
+++ src/components/ContactPage/Contact/Contact.tsx
@@ -8,22 +8,30 @@ const Contact = () => {
     return (
         <section className="contact">
             <div className="contact__container">
-                <ContactOption
-                    icon={<Phone className="contactoption__icon" />}
-                    text="+48 730 722 030"
-                    link={"tel:+48740722030"}
-                />
-                <ContactOption
-                    icon={<Envelope className="contactoption__icon" />}
-                    text="djaworskimusic@gmail.com"
-                    link={"mailto:djawroskimusic@gmail.com"}
-                />
+                <h2 className="contact__header">Kontakt</h2>
+                <h3 className="contact__text">
+                    Jeśli chcesz się ze mną skontaktować i omówić ofertę
+                    skorzystaj z poniższych informacji lub użyj formularza
+                    kontaktowego.
+                </h3>
+                <div className="contact__options">
+                    <ContactOption
+                        icon={<Phone className="contactoption__icon" />}
+                        text="+48 730 722 030"
+                        link={"tel:+48740722030"}
+                    />
+                    <ContactOption
+                        icon={<Envelope className="contactoption__icon" />}
+                        text="djaworskimusic@gmail.com"
+                        link={"mailto:djawroskimusic@gmail.com"}
+                    />
 
-                <ContactOption
-                    icon={<Instagram className="contactoption__icon" />}
-                    text="jaworskimusic"
-                    link={"https://instagram.com/jaworskimusic"}
-                />
+                    <ContactOption
+                        icon={<Instagram className="contactoption__icon" />}
+                        text="jaworskimusic"
+                        link={"https://instagram.com/jaworskimusic"}
+                    />
+                </div>
             </div>
         </section>
     );
