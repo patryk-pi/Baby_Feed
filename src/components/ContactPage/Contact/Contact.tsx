0d39ee79060f9cf1d8348cb44d62638d6925f2dc
patryk-pi <patryk.piasecki1@gmail.com>
2023-12-23 13:08:55 +0100

    created Phone icon component

--- src/components/ContactPage/Contact/Contact.tsx
+++ src/components/ContactPage/Contact/Contact.tsx
@@ -1,9 +1,16 @@
+import { text } from "stream/consumers";
 import ContactOption from "../ContactOption/ContactOption";
+import Envelope from "../Icons/Envelope";
 
 const Contact = () => {
     return (
         <section className="contact">
-            <div className="contact__container"></div>
+            <div className="contact__container">
+                <ContactOption
+                    icon={<Envelope className="contactoption__icon" />}
+                    text="djaworskimusic@gmail.com"
+                />
+            </div>
         </section>
     );
 };
