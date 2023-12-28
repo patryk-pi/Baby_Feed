ae48989afc6a450d67a017a5b3a51e71df6f2081
patryk-pi <patryk.piasecki1@gmail.com>
2023-12-28 14:08:18 +0100

    implemented sendEmail function in Form component

--- src/components/ContactPage/ContactForm/ContactForm.tsx
+++ src/components/ContactPage/ContactForm/ContactForm.tsx
@@ -1,12 +1,27 @@
 import { useFormik } from "formik";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
     const serviceID = process.env.SERVICE_ID as string;
     const templateID = process.env.TEMPLATE_ID as string;
     const key = process.env.PUBLIC_KEY as string;
 
+    const form = useRef<HTMLFormElement | null>(null);
+
     const sendEmail = (e: Event) => {
-        e.preventDefault;
+        e.preventDefault();
+
+        if (form.current) {
+            emailjs.sendForm(serviceID, templateID, form.current, key).then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        }
     };
 
     const formik = useFormik({
@@ -23,7 +38,7 @@ const ContactForm = () => {
 
     return (
         <section className="contactform">
-            <form onSubmit={formik.handleSubmit}>
+            <form ref={form} onSubmit={formik.handleSubmit}>
                 <div className="contactform__container"></div>
             </form>
         </section>
