1236c7fc862e5c9230e25c42199aca675068dc9b
patryk-pi <patryk.piasecki1@gmail.com>
2024-01-10 20:24:55 +0100

    created Offer component

--- src/app/oferta/page.tsx
+++ src/app/oferta/page.tsx
@@ -1,6 +1,14 @@
+import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
+import Offer from "@/components/OfferPage/Offer";
 
 const page = () => {
-    return <Navbar />;
+    return (
+        <>
+            <Navbar />
+            <Offer />
+            <Footer />
+        </>
+    );
 };
 export default page;
---
+++ src/components/OfferPage/Offer.tsx
@@ -0,0 +1,8 @@
+const Offer = () => {
+    return (
+        <section className="offer">
+            <div className="offer__container"></div>
+        </section>
+    );
+};
+export default Offer;
