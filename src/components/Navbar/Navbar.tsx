12a37e2bebff944f85b4a8b4daafee6a933d4300
patryk-pi <patryk.piasecki1@gmail.com>
2024-02-22 16:32:20 +0100

    added working mobile nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
                     </div>
                 </nav>
             </section>
-            <MobileNav />
+            {mobileOpen && <MobileNav />}
         </>
     );
 };
